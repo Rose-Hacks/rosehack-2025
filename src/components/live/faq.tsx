d4ba6181e284941a13748cffcb53780fce3c62eb
nidheesh-m-vakharia <nvakh003@ucr.edu>
2024-11-14 19:42:28 -0800

    FAQ headings added; content needs to be edited

--- src/components/live/faq.tsx
+++ src/components/live/faq.tsx
@@ -8,13 +8,14 @@ import { QUESTIONS } from "@/data/faq";
 
 const FAQ = () => {
   return (
-    <div className="flex w-full flex-col items-center">
-      <div className="w-10/12">
-        <Accordion type="single" collapsible className="w-full">
+    <div className="flex w-full flex-col text-rosehack-white items-center py-10 bg-rosehack-blue-100">
+      <h1 className="md:text-6xl text-4xl font-netron font-bold text-center mb-8">FAQ</h1>
+      <div className="max-w-[1040px] md:w-full w-11/12 ">
+        <Accordion type="single" collapsible className="w-full divide-y-2">
           {QUESTIONS.map(({ question, answer }, index) => (
-            <AccordionItem value={question} key={index}>
-              <AccordionTrigger>{question}</AccordionTrigger>
-              <AccordionContent>{answer}</AccordionContent>
+            <AccordionItem value={question} key={index} className=" text-rosehack-white md:text-lg text-sm">
+              <AccordionTrigger className="text-left ">{question}</AccordionTrigger>
+              <AccordionContent className="sm:text-[12px] md:text-lg">{answer}</AccordionContent>
             </AccordionItem>
           ))}
         </Accordion>
--- src/data/faq.ts
+++ src/data/faq.ts
@@ -5,22 +5,75 @@ type QuestionAnswer = {
 
 export const QUESTIONS: QuestionAnswer[] = [
   {
-    question: "What is hackathon website?",
+    question: "What is Rosehack?",
     answer:
       "This repo centralizes the backbone for all websites. It serves as a template for mainly the backend, user registration, user dashboard and and admin portal for organizers. The frontend is built out by respective hackathon committees.",
   },
   {
-    question: "How do I join the committee?",
+    question: "How can I apply for Rosehack?",
     answer:
       "No experience is required. You can reach out to us on discord and start working! We have weekly meetings and tasks via GitHub",
   },
   {
-    question: "How to get started with coding?",
+    question: "When are the applications due?",
     answer:
       "Attend weekly meetings for full onboarding. You can clone the repo, install necesssary packages with `npm i`(download node.js first). You would need to reach out to a swe lead for repo secrets file. Follow the steps on the readme for more.",
   },
   {
-    question: "How did we build out this robust system with so many services?",
+    question: "Where will the hackathon be held?",
+    answer:
+      "Check out `/engineering` to read up on the different technologies we used and the challenges we solved!",
+  },
+  {
+    question: "I have no experience, can I still apply?",
+    answer:
+      "Check out `/engineering` to read up on the different technologies we used and the challenges we solved!",
+  },
+  {
+    question: "How do I prepare for Rosehack?",
+    answer:
+      "Check out `/engineering` to read up on the different technologies we used and the challenges we solved!",
+  },
+  {
+    question: "Will food be provided? Will it be free or do I have to pay?",
+    answer:
+      "Check out `/engineering` to read up on the different technologies we used and the challenges we solved!",
+  },
+  {
+    question: "Are there prizes?",
+    answer:
+      "Check out `/engineering` to read up on the different technologies we used and the challenges we solved!",
+  },
+  {
+    question: "Does it cost money to join Rosehack?",
+    answer:
+      "Check out `/engineering` to read up on the different technologies we used and the challenges we solved!",
+  },
+  {
+    question: "How do I find my team?",
+    answer:
+      "Check out `/engineering` to read up on the different technologies we used and the challenges we solved!",
+  },
+  {
+    question: "How many members can be on a team?",
+    answer:
+      "Check out `/engineering` to read up on the different technologies we used and the challenges we solved!",
+  },
+  {
+    question:
+      "Do I have to join the discord server? What if I don't have discord?",
+    answer:
+      "Check out `/engineering` to read up on the different technologies we used and the challenges we solved!",
+  },
+  {
+    question:
+      "Do I have to design the whole time and submit a project at the end?",
+    answer:
+      "Check out `/engineering` to read up on the different technologies we used and the challenges we solved!",
+  },
+  {
+    question:
+      "My question isn't here.",
     answer:
       "Check out `/engineering` to read up on the different technologies we used and the challenges we solved!",
   },
