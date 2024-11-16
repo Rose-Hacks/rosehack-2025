78d269e26326ce52c41a991e748699bdd4f71a75
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-11-16 15:05:45 -0800

    created judges

--- src/components/live/title.tsx
+++ src/components/live/title.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Title = ({ title }: { title: string }) => {
-  return <div className="text-5xl font-normal">{title}</div>;
+  return (
+    <div className="font-netron text-3xl font-normal text-white md:text-6xl">
+      {title}
+    </div>
+  );
 };
 
 export default Title;
