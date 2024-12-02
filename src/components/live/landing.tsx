0f482ebe162e99ad2c830941c5fae796c1153c9c
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-12-01 19:34:57 -0800

    fixed mobile but its coded soo bad LOLOL

--- src/components/live/landing.tsx
+++ src/components/live/landing.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+import Content from "./landing/content";
+import Logo from "@/public/assets/landing/RHLogo.svg";
 import city1 from "@/public/assets/landing/citybg1.svg";
 import city2 from "@/public/assets/landing/citybg2.svg";
 import dome from "@/public/assets/landing/dome.svg";
@@ -13,14 +14,18 @@ import twintower2 from "@/public/assets/landing/tower2.svg";
 
 const Landing = () => {
   return (
-    <div className="flex w-full flex-col items-center justify-center bg-rosehack-blue-100">
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-rosehack-blue-100 md:h-full">
+      <div className="absolute top-5 z-50 flex w-fit flex-col items-center justify-center md:top-0 md:w-full">
+        <Image src={Logo} alt="Rose Hack Logo" className="w-1/4 md:w-fit" />
+        <Content />
+      </div>
       <Image
         src={clouds}
         alt="clouds"
         className="absolute right-0 top-0 w-full"
       />
       <div className="relative h-full">
-        <div className="relative -bottom-36 z-0">
+        <div className="relative -bottom-[50%] z-0 md:-bottom-36">
           <Image
             src={city2}
             alt="clouds"
@@ -28,26 +33,25 @@ const Landing = () => {
           />
           <Image src={city1} alt="clouds" className="relative z-10" />
         </div>
-
         <Image
           src={dome}
           alt="dome"
-          className="absolute -bottom-14 right-[10%] z-30 w-1/6 md:bottom-[10%] md:w-1/3"
+          className="-bottom-14 right-[5%] z-30 hidden w-1/6 md:absolute md:bottom-[10%] md:w-1/3"
         />
-        <div className="absolute left-[2%] flex md:top-[16%]">
+        <div className="absolute -left-[5%] top-[47.5%] flex w-full md:top-[15%]">
           <Image
             src={box}
             alt="dome"
-            className="relative left-[6.5%] top-4 z-30 w-[10%] md:left-[20%] md:top-14 md:w-fit"
+            className="relative left-[6.5%] top-4 z-30 w-[10%] md:left-[8%] md:top-14 md:w-fit"
           />
           <Image src={SpaceNeedle} alt="dome" className="z-30 w-1/5 md:w-fit" />
           <Image
             src={basic}
             alt="dome"
-            className="relative right-[6%] top-3 z-30 w-[11%] md:right-[18%] md:top-10 md:w-fit"
+            className="relative right-[6%] top-3 z-30 w-[11%] md:right-[7%] md:top-10 md:w-fit"
           />
         </div>
-        <div className="absolute -right-[25%] flex gap-3 md:right-[5%] md:top-[26%] md:gap-10">
+        <div className="absolute -right-[25%] top-[48.5%] flex gap-3 md:right-[5%] md:top-[26%] md:gap-10">
           <Image
             src={twintower1}
             alt="dome"
@@ -62,7 +66,7 @@ const Landing = () => {
         <Image
           src={grass}
           alt="grass"
-          className="relative -bottom-24 z-20 w-full md:bottom-0"
+          className="relative bottom-0 z-20 h-full w-full"
         />
       </div>
     </div>
