1f4e6a27ce299945a1954fce85d5dcfef1723291
shahdivyank <divyank.shah.2016@gmail.com>
2024-12-27 16:19:21 -0800

    changes

--- src/components/user/resources/resources.tsx
+++ src/components/user/resources/resources.tsx
@@ -9,23 +9,21 @@ const Resources = () => {
   const [search, setSearch] = useState(HACKPACKS);
 
   return (
-    <>
-      <div className="flex h-full flex-col gap-3 py-4">
-        <Label className="pr-5 text-2xl font-bold">Hackpacks</Label>
-        <Toolbar data={HACKPACKS} setSearch={setSearch} />
-        <div className="mt-8 grid grid-cols-4 gap-4">
-          {search.map(({ text, techs, link, description }, index) => (
-            <Hackpack
-              key={index}
-              text={text}
-              techs={techs}
-              link={link}
-              description={description}
-            />
-          ))}
-        </div>
+    <div className="flex h-full flex-col gap-3 py-4">
+      <Label className="pr-5 text-2xl font-bold">Hackpacks</Label>
+      <Toolbar data={HACKPACKS} setSearch={setSearch} />
+      <div className="mt-8 grid grid-cols-4 gap-4">
+        {search.map(({ title, languages, link, description }, index) => (
+          <Hackpack
+            key={index}
+            text={title}
+            languages={languages}
+            link={link}
+            description={description}
+          />
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
