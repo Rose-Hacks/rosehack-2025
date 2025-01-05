e8015e213b11495251bbb4f6d8072c2b009ea63a
SafeDuck <schopra579@gmail.com>
2025-01-04 16:08:45 -0800

    change badge shadcn

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -11,9 +11,9 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default:
-          "border-transparent text-slate-50 dark:bg-slate-50 dark:text-slate-900",
+          "border-transparent text-slate-50 bg-slate-400 dark:bg-slate-50 dark:text-slate-900",
         secondary:
-          "border-transparent bg-slate-100 text-slate-900  dark:bg-slate-800 dark:text-slate-50",
+          "border-transparent bg-slate-400 text-slate-900  dark:bg-slate-800 dark:text-slate-50",
         destructive:
           "border-transparent bg-red-500 text-slate-50 dark:bg-red-900 dark:text-slate-50",
         outline: "text-slate-950 dark:text-slate-50",
