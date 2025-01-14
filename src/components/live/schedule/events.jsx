da5ab9085d51698212c9946b1be710b17e6f2422
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-01-13 18:31:49 -0800

    init styling for new schedule

--- src/components/live/schedule/events.jsx
+++ src/components/live/schedule/events.jsx
@@ -10,66 +10,58 @@ const Events = ({ events, totalDays }) => {
         })
       : "Monday",
   );
-  const [selectedEvent, setSelectedEvent] = useState(null);
 
   return (
-    <div className="relative z-10">
-      <div className="m-2 flex w-fit items-center justify-center font-netron">
-        <div className="relative left-0 top-0 flex flex-col justify-start text-base md:text-2xl">
-          {totalDays.map((day) => (
-            <button
-              key={day}
-              className={`flex justify-center p-2 font-semibold text-rosehack-white focus:outline-none ${
-                selectedDay === day
-                  ? "sticky text-xl md:text-4xl"
-                  : "bg-transparent"
-              }`}
-              onClick={() => setSelectedDay(day)}
-            >
-              {day}
-            </button>
-          ))}
+    <div className="flex w-full flex-col items-center justify-center gap-10">
+      <div className="mx-auto grid w-10/12 grid-cols-7 items-center justify-between rounded-xl border-4 border-white font-netron text-base md:text-2xl">
+        {totalDays.map((day) => (
+          <button
+            key={day}
+            className={`flex justify-center rounded p-2 text-white duration-300 focus:outline-none ${
+              selectedDay === day ? "bg-gray-500" : "bg-gray-500/50"
+            }`}
+            onClick={() => setSelectedDay(day)}
+          >
+            {day}
+          </button>
+        ))}
+      </div>
+      <div className="mt-6 flex h-full w-9/12 flex-col gap-2 text-white">
+        <div className="grid w-full grid-cols-4 justify-between text-center font-semibold sm:text-sm md:text-xl">
+          <span className="">Time</span>
+          <span className="">Event</span>
+          <span className="">Type</span>
+          <span className="">Location</span>
         </div>
-        <div className="mt-6 flex h-full flex-col items-center">
-          {events.length === 0 ||
-          events.filter(({ day }) => day === selectedDay).length === 0 ? (
-            <div className="flex h-40 w-full items-center justify-center text-rosehack-white opacity-70">
-              No events for this day
-            </div>
-          ) : (
-            events
-              .filter(({ day }) => day === selectedDay)
-              .map(({ start, summary, description, location }, index) => (
-                <div
-                  key={index}
-                  onClick={() => setSelectedEvent(index)}
-                  className={`grid w-full grid-cols-2 items-center justify-center border-t-4 px-4 py-3 text-xs font-semibold md:w-8/12 md:text-lg ${
-                    selectedEvent === index
-                      ? "border-rosehack-darkgreen text-rosehack-darkgreen"
-                      : "border-rosehack-white text-rosehack-white"
-                  }`}
-                >
-                  <div>
-                    <p className="flex w-full justify-start whitespace-normal text-lg md:text-3xl">
-                      {summary}
-                    </p>
-                    <p className="flex justify-start opacity-70">
-                      {description.split("\n")[0].substr(1)}
-                    </p>
-                  </div>
-                  <div className="flex flex-col items-end">
-                    <p>
+        {console.log(events)}
+        <div className="w-full divide-y-4 rounded-xl border-4 border-white p-2">
+          {events
+            .filter(({ day }) => day === selectedDay)
+            .map(({ start, summary, description, location }, index) => (
+              <div key={index} className="flex flex-col items-center">
+                <div className="font-workSans m-3 grid w-full grid-cols-4 items-center justify-center rounded-xl bg-gradient-to-r from-rosehack-green to-rosehack-darkgreen px-4 py-3 text-lg font-semibold">
+                  <p className="flex justify-center">
+                    <span className="rounded-xl bg-rosehack-darkgreen p-2">
                       {new Date(start).toLocaleTimeString("en-US", {
                         hour: "2-digit",
                         minute: "2-digit",
                         timeZone: "America/Los_Angeles",
                       })}
-                    </p>
-                    <p>{location}</p>
-                  </div>
+                    </span>
+                  </p>
+                  <p className="flex w-full justify-center">{summary}</p>
+                  <p className="flex justify-center">
+                    {description.split("\n")[0].substr(1)}
+                  </p>
+                  <p className="flex justify-center">
+                    <span className="rounded-full bg-rosehack-green p-3">
+                      {location}
+                    </span>
+                  </p>
                 </div>
-              ))
-          )}
+                <div className="border-t- w-9/12 border-white" />
+              </div>
+            ))}
         </div>
       </div>
     </div>
