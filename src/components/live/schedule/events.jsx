447c6323d080c17465d7d29346a2b87185f8ba84
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-01-17 23:10:25 -0800

    fixed traffic light

--- src/components/live/schedule/events.jsx
+++ src/components/live/schedule/events.jsx
@@ -6,7 +6,7 @@ import cloud from "@/public/assets/schedule/clouds.svg";
 
 const Events = ({ events, totalDays }) => {
   const [selectedDay, setSelectedDay] = useState(
-    new Date() > new Date(events[0].start)
+    new Date() > new Date(events[0]?.start)
       ? new Date().toLocaleString("en-US", {
           timeZone: "America/Los_Angeles",
           weekday: "long",
