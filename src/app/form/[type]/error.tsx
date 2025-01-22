c24661ffde9cc956ca6f749b33aa14dd6a5bbe56
SafeDuck <schopra579@gmail.com>
2025-01-22 15:36:50 -0800

    text white

--- src/app/form/[type]/error.tsx
+++ src/app/form/[type]/error.tsx
@@ -3,7 +3,7 @@ import LOGO from "@/public/assets/logo.svg";
 import Image from "next/image";
 const InternalError = () => {
   return (
-    <div className="font-poppins flex w-full flex-col items-center justify-center gap-4 text-xl font-semibold lg:text-3xl">
+    <div className="font-poppins flex w-full flex-col items-center justify-center gap-4 text-xl font-semibold text-white lg:text-3xl">
       <Image src={LOGO} alt="Rosehack Logo" />
       <div className="w-5/6 text-center">
         We're sorry something unexpected happened
