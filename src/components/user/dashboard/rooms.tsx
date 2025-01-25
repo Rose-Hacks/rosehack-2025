cbd3029fd267a0220222e0601bc1e1494089be85
shahdivyank <divyank.shah.2016@gmail.com>
2025-01-24 17:32:18 -0800

    update hackrooms

--- src/components/user/dashboard/rooms.tsx
+++ src/components/user/dashboard/rooms.tsx
@@ -1,6 +1,6 @@
 import { Badge } from "@/components/ui/badge";
 
-const rooms: string[] = ["WCH 127", "WCH 110", "WCH 130", "Byte", "WCH 129"];
+const rooms: string[] = ["WCH 127", "WCH 129", "Bytes", "Bourns A171"];
 
 const Rooms = (): React.ReactNode => {
   return (
